438cd7aa56093ba97f5377380f4436719f4fce04
PavelTitov16 <pashoktitov16@gmail.com>
2022-08-18 16:19:01 +0300

    refactor: remove useless lines

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,6 @@ import { RouterModule, Routes } from '@angular/router';
 import { Paths } from '@core/models';
 import { AuthorizationComponent, DictionaryComponent, GamesComponent, HomeComponent, StatisticComponent } from '@feature/pages';
 
-
 const routes: Routes = [
   {path: Paths.Home, component: HomeComponent},
   {path: Paths.Authorization, component: AuthorizationComponent},
