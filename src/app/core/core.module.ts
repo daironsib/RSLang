9ffacf0fee2ea0e864c1e2c10149d87287611d57
Artem Sokolov <daironsib@gmail.com>
2022-08-21 16:56:17 +0200

    feat: add api service

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,18 +3,22 @@ import { CommonModule } from '@angular/common';
 import { NavigationPanelComponent } from './components/navigation-panel/navigation-panel.component';
 import { RouterModule } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
+import { ApiService } from './services/api.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     NavigationPanelComponent
   ],
   imports: [
+    HttpClientModule,
     CommonModule,
     RouterModule,
     MatButtonModule
   ],
   exports: [
     NavigationPanelComponent
-  ]
+  ],
+  providers: [ ApiService ]
 })
 export class CoreModule { }
