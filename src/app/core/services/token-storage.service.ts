75e12a7bd655b6c8498b4e8db0d9cc967c40c78f
Artem Sokolov <daironsib@gmail.com>
2022-08-21 17:24:39 +0200

    feat: add token storage service

---
+++ src/app/core/services/token-storage.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+
+const TOKEN_KEY = 'auth-token';
+const USER_KEY = 'auth-user';
+
+@Injectable()
+export class TokenStorageService {
+  constructor() {}
+
+  public signOut(): void {
+    window.localStorage.clear();
+  }
+
+  public saveToken(token: string): void {
+    window.localStorage.removeItem(TOKEN_KEY);
+    window.localStorage.setItem(TOKEN_KEY, token);
+  }
+
+  public getToken(): string | null {
+    return window.localStorage.getItem(TOKEN_KEY);
+  }
+
+  public saveUser(user: any): void {
+    window.localStorage.removeItem(USER_KEY);
+    window.localStorage.setItem(USER_KEY, JSON.stringify(user));
+  }
+
+  public getUser(): object {
+    const user = window.localStorage.getItem(USER_KEY);
+    if (user) {
+      return JSON.parse(user);
+    }
+    return {};
+  }
+}
