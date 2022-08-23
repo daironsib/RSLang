2e0bd72d71243f4ee833c632ead8545cc25f413f
Anna <annabutkova94@gmail.com>
2022-08-23 22:44:08 +0500

    refactor: do path shorter

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { NavigationPanelComponent } from '../navigation-panel/navigation-panel.component';
 
 @Component({
   selector: 'app-header',
@@ -10,7 +9,5 @@ export class HeaderComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NavigationPanelComponent } from './components/navigation-panel/navigation-panel.component';
+import { NavigationPanelComponent } from '@core/components/navigation-panel/navigation-panel.component';
 import { RouterModule } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { HttpClientModule } from '@angular/common/http';
-import { ApiService } from './services/api.service';
-import { TokenStorageService } from './services/token-storage.service';
-import { FooterComponent } from './components/footer/footer.component';
-import { FooterService } from './services/footer.service';
-import { HeaderComponent } from './components/header/header.component';
+import { ApiService } from '@core/services/api.service';
+import { TokenStorageService } from '@core/services/token-storage.service';
+import { FooterComponent } from '@core/components/footer/footer.component';
+import { FooterService } from '@core/services/footer.service';
+import { HeaderComponent } from '@core/components/header/header.component';
 
 @NgModule({
   declarations: [
