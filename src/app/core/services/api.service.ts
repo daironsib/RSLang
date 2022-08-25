49ad1b1d1161ac69a0eeec08ff95702c6185785a
PavelTitov16 <pashoktitov16@gmail.com>
2022-08-25 15:09:26 +0300

    fix: delete useless specs

--- src/app/core/services/api.service.ts
+++ src/app/core/services/api.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { TokenStorageService } from '@core/services/token-storage.service';
-import { ILogin, IRegister, ISettings, IStatistics, IUserWords, IWord } from '@core/models/api';
+import { ILogin, IRegister, ISettings, IStatistics, IUserWords, IWord } from '@core/models';
 
 @Injectable()
 export class ApiService {
