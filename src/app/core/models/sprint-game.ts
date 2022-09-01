c8a919008d2c4139387b005b708e97e9130810bb
PavelTitov16 <pashoktitov16@gmail.com>
2022-09-01 22:08:44 +0300

    feat: add game logic with score

---
+++ src/app/core/models/sprint-game.ts
@@ -0,0 +1,10 @@
+export interface SprintGameWord {
+    word: string,
+    translation: string,
+    correctTranslation: string
+}
+
+export interface SprintGameWordStatistic {
+    word: string,
+    isCorrectAnswer: boolean
+}
--- src/app/feature/pages/index.ts
+++ src/app/feature/pages/index.ts
@@ -1,5 +1,5 @@
 export * from './home/home.component';
-export * from './games/games.component';
+export * from './sprint-game/sprint-game.component';
 export * from './dictionary/dictionary.component';
 export * from './statistic/statistic.component';
 export * from './authorization/authorization.component';
