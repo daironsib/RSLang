c372663c67ef3d1037afd0fc9a032a4445e3b3a8
PavelTitov16 <pashoktitov16@gmail.com>
2022-09-03 03:55:48 +0300

    refactor: solve confclicts and add styles

--- src/app/core/models/keyEvents.ts
+++ src/app/core/models/keyEvents.ts
@@ -1,11 +1,11 @@
-  export enum KEY_CODE {
-    ENTER = 13,
-    SPACE = 32,
-    KEY1 = 49,
-    KEY2 = 50,
-    KEY3 = 51,
-    KEY4 = 52,
-    KEY5 = 53,
-    LEFT = 37,
-    RIGHT = 39
-  };
+export enum KEY_CODE {
+  ENTER = 13,
+  SPACE = 32,
+  KEY1 = 49,
+  KEY2 = 50,
+  KEY3 = 51,
+  KEY4 = 52,
+  KEY5 = 53,
+  RIGHT = 39,
+  LEFT = 37
+}
