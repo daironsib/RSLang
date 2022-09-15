20424c798aa35f2a8b26976cd9d491ef017747b5
Anna <annabutkova94@gmail.com>
2022-09-16 01:12:01 +0500

    feat: burger menu

--- src/app/core/components/navigation-panel/navigation-panel.component.ts
+++ src/app/core/components/navigation-panel/navigation-panel.component.ts
@@ -7,14 +7,13 @@ import { TokenStorageService } from '@core/services/token-storage.service';
   templateUrl: './navigation-panel.component.html',
   styleUrls: ['./navigation-panel.component.scss']
 })
-export class NavigationPanelComponent implements OnInit {
+export class NavigationPanelComponent {
   public paths = Paths;
   public user: IUserLS;
   public gameMenu: boolean = false;
+  public burgerMenu: boolean = false;
 
   constructor(public tokenStorageService: TokenStorageService) {
     this.user = this.tokenStorageService.getUser();
    }
-
-  ngOnInit(): void { }
 }
